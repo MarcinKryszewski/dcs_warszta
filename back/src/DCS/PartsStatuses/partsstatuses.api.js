f1f0f4abd58e1a4882705865095e1b6cda8ba303
marcin.kryszewski@onet.eu <marcin.kryszewski@onet.eu>
2023-03-12 23:01:42 +0100

    Created components PartsStatuses and TaskConfirms

---
+++ back/src/DCS/PartsStatuses/partsstatuses.api.js
@@ -0,0 +1,52 @@
+const PartsStatus = require('./partsstatuses.model');
+
+class PartsStatusActions {
+
+    async AllPartsStatuss(req, res) {
+        const partsStatuss = await PartsStatus.findAll();
+        res.status(200).send(partsStatuss);
+    }
+
+    async GetPartsStatus(req, res) {
+        const id = req.params.id;
+        const partsStatus = await PartsStatus.findOne({ where: { Id: id }});
+        res.status(200).json(partsStatus);
+    }
+
+    async AddPartsStatus(req, res) {
+        const taskId = req.body.TaskId;
+        const status = req.body.Status;
+        const personId = req.body.PersonId;
+        const date = req.body.Date;
+        const partsStatus = PartsStatus.build({
+            Id: null,
+            TaskId: taskId,
+            Status: status,
+            PersonId: personId,
+            Date: date
+        });
+        console.log(partsStatus);
+        await partsStatus.save();
+        res.status(200).send(partsStatus);
+    }
+
+    async UpdatePartsStatus(req, res) {
+        const id = req.params.id;
+        const partsStatus = await PartsStatus.findOne({ where: { Id: id }});
+        partsStatus.TaskId = req.body.TaskId;
+        partsStatus.Status = req.body.Status;
+        partsStatus.PersonId = req.body.PersonId;
+        partsStatus.Date = req.body.Date;
+        await partsStatus.save();
+        res.status(201).json(partsStatus);
+    }
+
+    async DeletePartsStatus(req, res) {
+        const id = parseInt(req.params.id);
+        const partsStatus = await PartsStatus.findOne({ where: { Id: id }});
+        await partsStatus.destroy();
+        res.sendStatus(204);
+    }
+}
+
+module.exports = new PartsStatusActions();
