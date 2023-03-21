46c06901b3bb61c1bcd1f89b52c979776243b99f
marcin.kryszewski@onet.eu <marcin.kryszewski@onet.eu>
2023-03-21 23:05:13 +0100

    Routing init

--- front/src/pages/Admin.jsx
+++ front/src/pages/Admin.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 import { PageContainer } from "../layouts/_layouts";
 import { Dashboard, Tables } from "../features/_features";
-
-//################TEMP###############
-
-import { Box } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
-
-//################TEMP###############
 
 function Admin() {
   return (
