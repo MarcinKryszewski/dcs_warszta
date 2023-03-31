73f686726800c24b8c8acef2fc935d9f14b16f6f
MarcinKryszewski <118294302+MarcinKryszewski@users.noreply.github.com>
2023-03-31 10:23:16 +0200

    Import default toolbar

--- front/src/components/_components.js
+++ front/src/components/_components.js
@@ -1,2 +1,5 @@
 export { default as Header } from "./Header";
 export * from "./Header";
+
+export { default as DefaultTableToolbar } from "./DefaultTableToolbar";
+export * from "./DefaultTableToolbar";
